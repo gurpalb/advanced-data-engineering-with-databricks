1ba56c10ff62e2475cccbc6d876d01fbd800e4c2
SireInsectus <SireInsectus>
2022-02-26 01:16:30 +0000

    Publishing v2.2.0

--- Advanced-Data-Engineering-with-Databricks-2022-02-11/Includes/module-1/setup-lesson-1.01.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-# MAGIC %run ../_databricks-academy-helper $lesson="1.01"
-
-# COMMAND ----------
-
-DA.cleanup()
-DA.init(create_db=False)
-DA.conclude_setup()
-
--- Advanced-Data-Engineering-with-Databricks-2022-02-11/Includes/module-4/setup-lesson-4.02.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-# MAGIC %run ../_databricks-academy-helper $lesson="4.02"
-
-# COMMAND ----------
-
-DA.cleanup()
-DA.init()
-DA.conclude_setup()
-
